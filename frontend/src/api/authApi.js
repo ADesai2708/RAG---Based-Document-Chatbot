aff4e66dcf84148a0d9b4c06c0c965c7619ccc90
Anjali Desai <anjali_desai@srmap.edu.in>
2026-06-09 14:39:05 +0530

    frontend begin

---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,19 @@
+import api from "../services/axios";
+
+export const loginUser =
+(data)=>{
+
+ return api.post(
+   "/auth/login",
+   data
+ );
+};
+
+export const registerUser =
+(data)=>{
+
+ return api.post(
+   "/auth/register",
+   data
+ );
+};
